12ed2e6a48b9e0df2c28fe6c8d763e2058ef7236
ivan <pvana621@gmail.com>
2026-09-01 10:14:58 +0300

    start

---
+++ my_project/target/debug/deps/my_project-8cb895159ab98411.d
@@ -0,0 +1,5 @@
+D:\AAAAAA\informatic\semestr_5\computer graphics\SimpleDimple-main\my_project\target\debug\deps\my_project-8cb895159ab98411.d: src\main.rs
+
+D:\AAAAAA\informatic\semestr_5\computer graphics\SimpleDimple-main\my_project\target\debug\deps\libmy_project-8cb895159ab98411.rmeta: src\main.rs
+
+src\main.rs:
