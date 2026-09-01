084a8a63336252b9c7d46bafa425a6f1b29126d0
ivan <pvana621@gmail.com>
2026-09-01 10:16:08 +0300

    clear

--- my_project/target/debug/deps/my_project-904b467464649f32.d
+++
@@ -1,5 +0,0 @@
-D:\AAAAAA\informatic\semestr_5\computer graphics\SimpleDimple-main\my_project\target\debug\deps\my_project-904b467464649f32.d: src\main.rs
-
-D:\AAAAAA\informatic\semestr_5\computer graphics\SimpleDimple-main\my_project\target\debug\deps\libmy_project-904b467464649f32.rmeta: src\main.rs
-
-src\main.rs:
